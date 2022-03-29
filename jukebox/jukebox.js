ca92af7e2ccec96589efc5f4bc573dd1d1ef098f
Lauren-Lin <63761042+Lauren-Lin@users.noreply.github.com>
2022-03-28 20:02:14 -0400

    Uploaded Initial Jukebox Jamboree Files

---
+++ jukebox/jukebox.js
@@ -0,0 +1,147 @@
+music = [{
+        "genre": "Kpop",
+        "Gangnam Style": {
+            "name": "Gangnam Style",
+            "artist": "PSY",
+            "album": "The Singles",
+            "year": "2013",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/a5/9e/54/a59e5438-4b4d-c7f5-7b97-e9ed49ac115b/mzaf_13985237334028592858.plus.aac.p.m4a",
+        },
+        "Polaroid Love": {
+            "name": "Polaroid Love",
+            "artist": "ENHYPEN",
+            "album": "DIMENSION : ANSWER",
+            "year": "2022",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview126/v4/aa/5f/ce/aa5fceb0-565b-eec1-ab04-9e74e6e980e7/mzaf_18234914765732929576.plus.aac.p.m4a",
+        },
+        "As If It's Your Last": {
+            "name": "As If It's Your Last",
+            "artist": "BlackPink",
+            "album": "Blackpink in Your Area",
+            "year": "2018",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/97/44/12/974412d8-624e-7a85-25b5-a4a0ac135299/mzaf_10160984043858145771.plus.aac.p.m4a",
+        },
+        "Good Day": {
+            "name": "Good Day",
+            "artist": "IU",
+            "album": "Real",
+            "year": "2015",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/e9/40/15/e940150d-0cde-c74e-893c-ddab87f921c7/mzaf_17571703730484332698.plus.aac.p.m4a",
+        }
+    },
+    {
+        "genre": "Electronic",
+        "Bangarang": {
+            "name": "Bangarang",
+            "artist": "Skrillex",
+            "album": "4th extended play",
+            "year": "2011",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/66/ab/1f/66ab1fcc-9b5c-a62b-da7c-3e6cf71fba77/mzaf_7713625038867859826.plus.aac.p.m4a",
+        },
+        "Wake Me Up": {
+            "name": "Wake Me Up",
+            "artist": "Avicii",
+            "album": "True",
+            "year": "2013",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/09/c9/42/09c9429e-7de4-e4de-0c2b-8414255f2015/mzaf_10772852223731885360.plus.aac.p.m4a",
+        },
+        "Electric Feel": {
+            "name": "Electric Feel",
+            "artist": "MGMT",
+            "album": "Oracular Spectacular",
+            "year": "2007",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/52/82/5a/52825a38-5ed1-0af3-5ca1-665e36c21e47/mzaf_14275750110061202362.plus.aac.p.m4a",
+        },
+        "Clarity": {
+            "name": "Clarity",
+            "artist": "Zedd (ft. Foxes)",
+            "album": "Clarity",
+            "year": "2012",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/4a/6e/ad/4a6ead9b-c7e6-d04f-4962-3a227344d9ea/mzaf_2358520479212825606.plus.aac.p.m4a"
+        }
+    },
+    {
+        "genre": "Pop",
+        "Party in the U.S.A.": {
+            "name": "Party in the U.S.A.",
+            "artist": "Miley Cyrus",
+            "album": "The Time Of Our Lives",
+            "year": "2009",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/84/2b/47/842b4707-1c2d-3448-850b-c574ed9adf59/mzaf_1527995791329548619.plus.aac.p.m4a",
+        },
+        "Locked Out of Heaven": {
+            "name": "Locked Out of Heaven",
+            "artist": "Bruno Mars",
+            "album": "Unorthadox Jukebox",
+            "year": "2012",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/f4/37/44/f4374481-e6e8-54d1-32ad-893ec2f4d495/mzaf_3915415747653767603.plus.aac.p.m4a",
+        },
+        "Baby One More Time": {
+            "name": "Baby One More Time",
+            "artist": "Britney Spears",
+            "album": "Baby One More Time",
+            "year": "1999",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/a9/35/0b/a9350b06-84e3-fe52-3463-c412a74b9778/mzaf_15738949959443199326.plus.aac.p.m4a",
+        },
+        "Single Ladies": {
+            "name": "Single Ladies",
+            "artist": "Beyonce",
+            "album": "I am...Sasha Fierce",
+            "year": "2008",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/83/ae/a6/83aea69b-303c-4e22-fdd8-8ffb3557abd8/mzaf_5568644041921528008.plus.aac.p.m4a",
+        }
+    },
+    {
+        "genre": "Disney",
+        "Let It Go": {
+            "name": "Let It Go",
+            "artist": "Kristen Bell",
+            "album": "Frozen Sountrack",
+            "year": "2013",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/5f/9b/11/5f9b1145-46f7-aa0a-4a5d-5666f31b6a36/mzaf_15812089719083089.plus.aac.p.m4a",
+        },
+        "Can You Feel the Love Tonight": {
+            "name": "Can You Feel the Love Tonight",
+            "artist": "Elton John",
+            "album": "The Lion King Soundtrack",
+            "year": "1994",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/f5/9c/23/f59c2357-241a-26d1-564b-26be4e2b734e/mzaf_12340032002066150037.plus.aac.p.m4a",
+        },
+        "How Far I'll Go": {
+            "name": "How Far I'll Go",
+            "artist": "Auli'i Cravalho",
+            "album": "Moana Sountrac",
+            "year": "2016",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2a/30/c2/2a30c27c-59f9-c4cb-d2e6-9097f0fb11f1/mzaf_16635287530021288067.plus.aac.p.m4a",
+        },
+        "Zero": {
+            "name": "Zero",
+            "artist": "Imagine Dragons",
+            "album": "Origins",
+            "year": "2018",
+            "link": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/01/7f/93/017f939a-c562-420a-73b8-0c83ed6d04f6/mzaf_888883585486301405.plus.aac.p.m4a",
+        }
+    }
+]
+
+array = [
+    "What is the name of the song?", "What is the name of the artist?", "What is the name of the album?", "What is the year the song came out?"
+]
+
+let board = document.querySelector('#jeopardyBoard');
+for (let i = 0; i < 4; i++) {
+    board.innerHTML += "<div class='genre'>" + music[i].genre + "</div>";
+}
+
+for (let i = 100; i < 500; i+=100) {
+    for (let j = 0; j < 4; j++) {
+        board.innerHTML += "<div class='points'>" + i + "<div class='question'>" + array[i/100] + "</div></div>";
+        
+    }
+}
+
+
+function myFunction() {
+    var popup = document.getElementById("#question");
+    popup.classList.toggle("show");
+}
